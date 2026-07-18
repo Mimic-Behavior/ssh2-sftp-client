33b5d329820fa6a26d0b5fb6117e492ffe144a9e
Aleksei Saenko <156539224+asaenko01@users.noreply.github.com>
2026-07-18 22:58:56 +0300

    Refactor SftpClient to improve error handling and add put method for file uploads

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Client, utils } from '@mimic-behavior/ssh2'
+import fs from 'node:fs'
 import path from 'node:path'
 
 import * as sftpUtils from './utils'
@@ -8,7 +9,7 @@ class SftpClient extends Client {
         const normalizedPath = path.posix.normalize(remotePath)
 
         if (normalizedPath === '.' || normalizedPath === '/') {
-            return
+            throw new Error(`Invalid remote path: ${normalizedPath}`)
         }
 
         if (!options.recursive) {
@@ -20,20 +21,60 @@ class SftpClient extends Client {
         for (const part of normalizedPath.split('/').filter(Boolean)) {
             currentPath = path.posix.join(currentPath, part)
 
-            try {
-                const stats = await super.stat(currentPath)
+            const result = await sftpUtils.attempt(() => super.mkdir(currentPath))
 
-                if (!stats.isDirectory()) {
+            if (sftpUtils.isSftpError(result) && result.code === utils.sftp.STATUS_CODE.FAILURE) {
+                const stats = await sftpUtils.attempt(() => super.stat(currentPath))
+
+                if (stats instanceof Error) {
+                    throw stats
+                }
+
+                if (stats.isDirectory() === false) {
                     throw new Error(`Path exists and is not a directory: ${currentPath}`)
                 }
-            } catch (err) {
-                if (sftpUtils.isSftpError(err) && err.code === utils.sftp.STATUS_CODE.NO_SUCH_FILE) {
-                    await super.mkdir(currentPath)
-                } else {
-                    throw err
-                }
+
+                continue
+            }
+
+            if (result instanceof Error) {
+                throw result
             }
         }
+    }
+
+    async put(localPath: string, remotePath: string): Promise<void> {
+        const normalizedPath = path.posix.normalize(remotePath)
+
+        if (normalizedPath === '.' || normalizedPath === '/') {
+            throw new Error(`Invalid remote path: ${normalizedPath}`)
+        }
+
+        const stats = await sftpUtils.attempt(() => fs.promises.stat(localPath))
+
+        if (stats instanceof Error) {
+            throw stats
+        }
+
+        if (stats && stats.isDirectory()) {
+            throw new Error('Local path is a directory')
+        }
+
+        const rStream = fs.createReadStream(localPath)
+        const wStream = await super.createWriteStream(normalizedPath)
+
+        return new Promise((resolve, reject) => {
+            function onError(err: Error) {
+                rStream.destroy()
+                wStream.destroy()
+                reject(err)
+            }
+
+            rStream.on('error', onError)
+            wStream.on('error', onError)
+            wStream.once('close', resolve)
+            rStream.pipe(wStream)
+        })
     }
 }
 
